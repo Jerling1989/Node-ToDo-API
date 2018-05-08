3dc3057f1bc69cdd465b25d341877e1bda57d1e4
Jacob <jerling1989@gmail.com>
2018-05-08 00:27:37 -0400

    ES6 object destructoring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,8 @@
 // REQUIRE MONGOCLIENT
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // This is a better way than above with ES6 object destructoring
+
+
 // CREATE MONGO DATABASE CONNECTION
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	// IF THERES A CONNECTION ERROR LOG MESSAGE ON CONSOLE
@@ -23,20 +26,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	// 	}
 	// });
 
-	// INSERT A DOCUMENT INTO THE USERS COLLECTION
-	db.collection('Users').insertOne({
-		name: 'Jacob',
-		age: 28,
-		location: 'New Jersey'
-	}, (err, result) => {
-		// IF THERE IS AN ERROR LOG THE ERROR MESSAGE TO THE CONSOLE
-		if (err) {
-			return console.log('Unable to insert User', err);
-			// ELSE LOG THE NEW DOCUMENT TO THE CONSOLE
-		} else {
-			console.log(JSON.stringify(result.ops, undefined, 2));
-		}
-	});
+	// // INSERT A DOCUMENT INTO THE USERS COLLECTION
+	// db.collection('Users').insertOne({
+	// 	name: 'Jordan',
+	// 	age: 31,
+	// 	location: 'California'
+	// }, (err, result) => {
+	// 	// IF THERE IS AN ERROR LOG THE ERROR MESSAGE TO THE CONSOLE
+	// 	if (err) {
+	// 		return console.log('Unable to insert User', err);
+	// 		// ELSE LOG THE NEW DOCUMENT TO THE CONSOLE
+	// 	} else {
+	// 		console.log(result.ops[0]._id.getTimestamp());
+	// 	}
+	// });
+
 	// DISCONNECT FROM MONGO DATABASE
 	client.close();
 });
