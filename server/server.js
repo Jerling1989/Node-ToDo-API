5472be4aaa03058084a08217a8e59dd675d90870
Jacob <jerling1989@gmail.com>
2018-05-09 02:21:36 -0400

    post /todos endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,33 @@
+// REQUIRE NPM PACKAGES
+var express = require('express');
+var bodyParser = require('body-parser');
+
 // REQUIRE MONGOOSE
 var {mongoose} = require('./db/mongoose');
 // REQUIRE MODELS
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+// CREATE APP VARIABLE
+var app = express();
+// USE BODYPARSER MIDDLEWARE
+app.use(bodyParser.json());
+
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+
+// SET PORT
+app.listen(3000, () => {
+	console.log('Running on port 3000');
+});
