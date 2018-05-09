c0f15cd8ef42cc4830868fb71610f83f3ffbbeb0
Jacob <jerling1989@gmail.com>
2018-05-09 16:57:59 -0400

    change database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 // SET MONGOOSE PROMISES
 mongoose.Promise = global.Promise;
-// CONNECT TO DATABASE
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// CONNECT TO DATABASE (HEROKU OR LOCALHOST)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 // EXPORT MONGOOSE VARIABLE
 module.exports = {mongoose};
